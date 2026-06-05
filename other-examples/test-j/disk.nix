e63e93b5eda2d6016cbbb9f1978e38573392dfd2
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-06-05 22:22:26 +0100

    example disko

---
+++ other-examples/test-j/disk.nix
@@ -0,0 +1,87 @@
+# Just an example
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme1n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              priority = 1;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["fmask=0077" "dmask=0077"];
+                extraArgs = ["-n" "NIXBOOT"];
+              };
+            };
+            swap = {
+              size = "40G";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true;
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+                extraArgs = ["-L" "NIXROOT"];
+              };
+            };
+          };
+        };
+      };
+
+      storage = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/home/cheryllamb/storage";
+                extraArgs = ["-L" "STORAGE"];
+              };
+            };
+          };
+        };
+      };
+
+      external = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/storage";
+                extraArgs = ["-L" "BIG_STORAGE"];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+  systemd.tmpfiles.rules = [
+    "d /home/cheryllamb/storage 0755 cheryllamb users - -"
+    "d /storage 0755 cheryllamb users - -"
+  ];
+}
